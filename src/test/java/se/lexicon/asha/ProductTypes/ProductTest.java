088b6c78f2cfcf225bee63bef2a3c596b0a041e7
asha joseph <ashajo183@gmail.com>
2021-01-13 09:34:30 +0100

    ProductTypes

---
+++ src/test/java/se/lexicon/asha/ProductTypes/ProductTest.java
@@ -0,0 +1,24 @@
+package se.lexicon.asha.ProductTypes;
+
+import junit.framework.TestCase;
+
+public class ProductTest extends TestCase {
+
+    public void testTestGetName() {
+    }
+
+    public void testTestSetName() {
+    }
+
+    public void testGetPrice() {
+    }
+
+    public void testSetPrice() {
+    }
+
+    public void testPurchase() {
+    }
+
+    public void testExamine() {
+    }
+}
