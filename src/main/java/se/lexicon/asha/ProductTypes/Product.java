3d3e2f1b669bf17e16c8b67ce6e1caf114b358b7
asha joseph <ashajo183@gmail.com>
2021-01-13 09:47:19 +0100

    ProductTypes

---
+++ src/main/java/se/lexicon/asha/ProductTypes/Product.java
@@ -0,0 +1,35 @@
+package se.lexicon.asha.ProductTypes;
+
+public abstract class Product {
+    String name;
+    int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    abstract Product purchase(int money);
+
+    abstract java.lang.String examine();
+
+
+}
+
+
